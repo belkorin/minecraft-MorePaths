a39492a412d6def872c8b43e9d270446fa1534f1
Belkorin <6710976+belkorin@users.noreply.github.com>
2021-08-30 21:09:50 -0400

    Version bump

--- src/main/java/net/arcanecitadel/morepaths/ModLoader.java
+++ src/main/java/net/arcanecitadel/morepaths/ModLoader.java
@@ -17,7 +17,7 @@ import net.arcanecitadel.morepaths.registries.ItemRegistry;
 
 		public static final String MODID = "morepaths";
 		public static final String NAME = "More Paths";
-		public static final String VERSION = "1.15.2-1.0.1";
+		public static final String VERSION = "1.15.2-1.2.0";
 	    public static Logger logger = LogManager.getLogger();
 
 
