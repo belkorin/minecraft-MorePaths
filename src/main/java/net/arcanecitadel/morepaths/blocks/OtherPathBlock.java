0c8be8f1a978c60af1b3e8395a3f986c3c95b62e
Belkorin <6710976+belkorin@users.noreply.github.com>
2021-09-24 02:01:27 -0400

    Added shovel harvest tool for the path blocks

--- src/main/java/net/arcanecitadel/morepaths/blocks/OtherPathBlock.java
+++ src/main/java/net/arcanecitadel/morepaths/blocks/OtherPathBlock.java
@@ -30,70 +30,74 @@ public class OtherPathBlock extends GrassPathBlock {
 
 	private Block _pathedFrom;
 	private boolean _isFallingBlock;
-	
+
 	public OtherPathBlock(Block pathedFrom) {
 		super(getProperties(pathedFrom));
 		_pathedFrom = pathedFrom;
 		_isFallingBlock = pathedFrom instanceof FallingBlock;
 	}
-	
-	//copy the basic properties, without ticking randomly
-	private static Properties getProperties(Block pathedFrom)
-	{
+
+	// copy the basic properties, without ticking randomly
+	private static Properties getProperties(Block pathedFrom) {
 		BlockState state = pathedFrom.getDefaultState();
-		
-		Properties newPathProperties = AbstractBlock.Properties.create(state.getMaterial(), state.getMaterialColor(null, null))
-		.hardnessAndResistance(state.getBlockHardness(null, null)).sound(state.getSoundType());
-		
+
+		Properties newPathProperties = AbstractBlock.Properties
+				.create(state.getMaterial(), state.getMaterialColor(null, null))
+				.hardnessAndResistance(state.getBlockHardness(null, null)).sound(state.getSoundType())
+				.harvestTool(ToolType.SHOVEL);
+
 		return newPathProperties;
 	}
-	
-	@Override
-	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-		if (checkIfFalling(worldIn, currentPos)) {
-	         worldIn.getPendingBlockTicks().scheduleTick(currentPos, this, 1);
-	      }
 
-	      return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+	@Override
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn,
+			BlockPos currentPos, BlockPos facingPos) {
+		if (checkIfFalling(worldIn, currentPos)) {
+			worldIn.getPendingBlockTicks().scheduleTick(currentPos, this, 1);
+		}
+
+		return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
 	}
 
-	
 	@Override
 	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
 		worldIn.setBlockState(pos, nudgeEntitiesWithNewState(state, _pathedFrom.getDefaultState(), worldIn, pos));
 	}
-	
+
 	@Override
-	   public BlockState getStateForPlacement(BlockItemUseContext context) {
-	      return !this.getDefaultState().isValidPosition(context.getWorld(), context.getPos()) ? Block.nudgeEntitiesWithNewState(this.getDefaultState(), _pathedFrom.getDefaultState(), context.getWorld(), context.getPos()) : super.getStateForPlacement(context);
-	   }
-	
-	private boolean checkIfFalling(IWorld worldIn, BlockPos currentPos)
-	{
-		if(!_isFallingBlock)
-			return false;
-		
-		return worldIn.isAirBlock(currentPos.down()) || FallingBlock.canFallThrough(worldIn.getBlockState(currentPos.down())) && currentPos.getY() >= 0;
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return !this.getDefaultState().isValidPosition(context.getWorld(), context.getPos())
+				? Block.nudgeEntitiesWithNewState(this.getDefaultState(), _pathedFrom.getDefaultState(),
+						context.getWorld(), context.getPos())
+				: super.getStateForPlacement(context);
 	}
-	
+
+	private boolean checkIfFalling(IWorld worldIn, BlockPos currentPos) {
+		if (!_isFallingBlock)
+			return false;
+
+		return worldIn.isAirBlock(currentPos.down())
+				|| FallingBlock.canFallThrough(worldIn.getBlockState(currentPos.down())) && currentPos.getY() >= 0;
+	}
+
 	@SubscribeEvent()
 	public static void doPlayerHarvestCheck(PlayerEvent.HarvestCheck harvestEvent) {
 		PlayerEntity p = harvestEvent.getPlayer();
-		
+
 		BlockState block = harvestEvent.getTargetBlock();
 		Block snowPath = BlockRegistry.SNOW_PATH.get();
-		
-		if(block.getBlock() != snowPath)
+
+		if (block.getBlock() != snowPath)
 			return;
 
 		ItemStack held = p.getHeldItemMainhand();
-		
+
 		Item item = held.getItem();
 		boolean isShovel = item.getToolTypes(held).contains(ToolType.SHOVEL);
-		
-		if(!isShovel)
+
+		if (!isShovel)
 			return;
-		
+
 		harvestEvent.setCanHarvest(true);
 	}
 
